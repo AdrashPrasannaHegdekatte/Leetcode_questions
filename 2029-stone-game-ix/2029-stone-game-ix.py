f842bddca3ca530b76d36899e9acdffcc3685276
AdrashPrasannaHegdekatte <adarshhegdekatte@gmail.com>
2026-08-16 10:44:14 +0530

    Time: 71 ms (14.29%), Space: 20.1 MB (100%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def stoneGameIX(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: bool
+        """
+        c0,c1,c2=0,0,0
+
+        for s in stones:
+            r=s%3
+            if r==0:c0+=1
+            elif r==1:c1+=1
+            else:c2+=1
+        if c0%2==0:return c1>0 and c2>0
+        return abs(c1-c2)>2
+
+        

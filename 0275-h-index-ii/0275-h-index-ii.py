41cf126476c46ea24b1e7f83d19e993a5fc57a4f
AdrashPrasannaHegdekatte <adarshhegdekatte@gmail.com>
2026-09-07 20:52:14 +0530

    Time: 0 ms (100%), Space: 14.8 MB (61.38%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n=len(citations)
+        l=0
+        r=n-1
+        while(l<=r):
+            mid=l+(r-l)//2
+            if citations[mid]>=n-mid:
+                r=mid-1
+            else:
+                l=mid+1
+        return n-l
+                
+
+
+
+        
